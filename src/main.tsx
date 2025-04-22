51af1c35d53c08f349c994a6893e180e3f1375b9
dollyca <109392596+dollyca@users.noreply.github.com>
2025-04-22 15:29:40 -0700

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import Customers from "./pages/Customers"
+import KycForm from "./pages/KycForm"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/kyc" element={<KycForm />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
