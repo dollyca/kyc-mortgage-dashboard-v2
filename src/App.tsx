909d937153c9e653cc3b2751f54ce1bc8960a01b
dollyca <109392596+dollyca@users.noreply.github.com>
2025-04-22 14:53:38 -0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Customers from "./pages/Customers";
-import KycForm from "./pages/KycForm";
+import Dashboard from "./pages/Dashboard.tsx";
+import Customers from "./pages/Customers.tsx";
+import KycForm from "./pages/KycForm.tsx";
 
 export default function App() {
   return (
