e68c9dd683ad12058e32ddd56b9de856a34a8e78
dollyca <109392596+dollyca@users.noreply.github.com>
2025-04-22 16:08:22 -0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard.tsx";
+import Dashboard from "./pages/Dashboard";
 import Customers from "./pages/Customers";
 import KycForm from "./pages/KycForm";
 export default function App() {
