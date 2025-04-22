901e461b6915c0ce2f2cd2ece34e43caebe1ce33
dollyca <109392596+dollyca@users.noreply.github.com>
2025-04-22 16:03:26 -0700

    Delete src/pages/Customers.js

--- src/pages/Customers.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import Layout from "../layout/Layout";
-export default function Customers() {
-    return (_jsxs(Layout, { children: [_jsx("h1", { className: "text-2xl font-bold mb-4", children: "\uD83D\uDC65 Customers Page" }), _jsxs("div", { className: "grid grid-cols-1 md:grid-cols-2 gap-4", children: [_jsx("div", { className: "bg-white p-4 shadow rounded-xl", children: "\u5BA2\u6236\u6578\u64DA\u7D71\u8A08" }), _jsx("div", { className: "bg-white p-4 shadow rounded-xl", children: "\u5BA2\u6236\u4E92\u52D5\u7D00\u9304" })] })] }));
-}
